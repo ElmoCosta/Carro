0ad22761abafec564b3ff5e6c9db113b424019d3
elmo costa <elmo.mascosta@gmail.com>
2024-04-29 08:02:31 -0300

    Primeira commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class PedidoCompra
+{
+      public int numero {get; set;}
+      public string produto {get; set;}
+      public int qtde {get; set;}
+      public double preco {get; set;}
+      public double desconto {get; set;}
+      public double total {get; set;}
+ 
+      public void CalcularPedido(int q, double p, double d)
+      {
+             this.qtde = q;
+             this.preco = p;
+             this.desconto = d;
+             this.total = q * p - d;
+      }
+}
+ 
+ 
+class Program
+{
+      static void Main(string[] args)
+      {
+             PedidoCompra p = new PedidoCompra();
+             p.CalcularPedido(3, 20.00, 5.00);
+             Console.WriteLine(p.total);
+      }
+}
